d9a096193971fadd98b77abdde9e1588a41d383f
Chinenye <83243717+Birdie13-stack@users.noreply.github.com>
2023-07-14 05:55:30 -0700

    Updated mobile version

---
+++ index.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Toggle the menu on click of the hamburger icon
+  document.querySelector(".hamburger").addEventListener("click", function () {
+    document.querySelector(".nav").classList.toggle("active");
+  });
+});
