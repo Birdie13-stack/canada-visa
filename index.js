e22a29765a52321fb74be6fa42058469a8c4941b
Chinenye <83243717+Birdie13-stack@users.noreply.github.com>
2023-07-15 10:49:08 -0700

    Made Updates

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function() {
   // Toggle the menu on click of the hamburger icon
   document.querySelector(".hamburger").addEventListener("click", function () {
     document.querySelector(".nav").classList.toggle("active");
